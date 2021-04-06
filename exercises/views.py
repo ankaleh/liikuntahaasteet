667c98a5adfe2c0f38bbdec94d45bec2b59f2751
Anna Lehto <anviinamaki@gmail.com>
2021-04-06 08:12:57 +0300

    Add login view.

--- exercises/views.py
+++ exercises/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+import os
 
 def index(request):
     return HttpResponse("Hei, maailma!")
+
+@login_required
+def secret(request):
+    return HttpResponse("Salainen sivu")
--- tietokantasovellus/urls.py
+++ tietokantasovellus/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 import exercises.views
 
 urlpatterns = [
-    path("", exercises.views.index, name="index"),
+    path('', exercises.views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('kirjaudu/', auth_views.LoginView.as_view(template_name='login.html')),
+	path('kirjaudu-ulos/', auth_views.LogoutView.as_view(next_page='/')),
+
+    path('salainen/', exercises.views.secret, name='secret')
 ]
